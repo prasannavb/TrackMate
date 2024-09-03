38e909ea7148d207215d58b4b50006c4cf76a480
Prasanna V B <prasannavb04@gmail.com>
2024-09-03 20:38:32 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./Home/Home";
+
+const App=()=>
+{
+  return(
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Home/Home.jsx
@@ -0,0 +1,77 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [date, setDate] = useState();
+  const [attendanceData, setAttendanceData] = useState([[]]);
+  const [fetched, setFetched] = useState(false);
+  const monthArray = [
+    "January", "February", "March", "April", "May", "June", 
+    "July", "August", "September", "October", "November", "December"
+  ];
+
+  const markAttendance = async () => {
+    if (date) {
+      const {data} = await axios.post('http://localhost:8000/', { date });
+      alert(`${data.message}`)
+      fetchAttendance() 
+    } else {
+      alert("Choose a date");
+    }
+  };
+
+  const handleDateChange = (e) => {
+    const { value } = e.target;
+    setDate(value);
+  };
+
+  const fetchAttendance = async () => {
+    const { data } = await axios.get('http://localhost:8000/fetchAttendance');
+    const newData = [];
+
+    for (let i = 0; i < 12; i++) {
+      newData.push(
+        data.AttendanceData.filter((entry) => entry.month === monthArray[i])
+      );
+    }
+
+    setAttendanceData(newData);
+    setFetched(true);
+  };
+
+  useEffect(()=>{
+    fetchAttendance();
+  },[])
+
+  return (
+    <>
+      <input type="date" name="date" onChange={handleDateChange} />
+      <button  onClick={markAttendance}>Present</button>
+      {/* <button onClick={fetchAttendance}>Fetch</button> */}
+
+      <table border={1}>
+        {fetched ? (
+          <tbody>
+            {attendanceData.map((monthData, index) => (
+              <tbody key={index}>
+               <tr>
+               <td>{monthArray[index]}</td>
+                <tbody>
+                {monthData.map((entry, subIndex) => (
+                  <td key={subIndex}>{entry.date}</td>
+                ))}
+                </tbody>
+                <td>{monthData.length}</td>
+               </tr>
+              </tbody>
+            ))}
+          </tbody>
+        ) : (
+          <></>
+        )}
+      </table>
+    </>
+  );
+};
+
+export default Home;
